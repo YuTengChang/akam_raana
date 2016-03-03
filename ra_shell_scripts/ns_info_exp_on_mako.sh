da43f8072d8eeaf81aa7d7ba85c4f1ef6ea1f30a
yutengchang <yutengchang@gmail.com>
2016-03-03 12:04:49 -0500

    for experiments on mako updates remove md5sum

--- ra_shell_scripts/ns_info_exp_on_mako.sh
+++ ra_shell_scripts/ns_info_exp_on_mako.sh
@@ -7,8 +7,8 @@ temp_file="/home/ychang/tmp/ns_exp.txt"
 output_file="/home/ychang/tmp/ns_exp_all.ext"
 
 # get the checksum of the particular mako result
-/home/ychang/bin/mako --csv -s "select * from (select * from ns_info where COUNTRY='JP' order by demand desc) where rownum<=20" > $temp_file
-md5str=`/usr/bin/md5sum $temp_file | awk '{print $1}'`
+/home/ychang/bin/mako --csv -s "select sum(demand) from ns_info where COUNTRY='JP' " | tail -1 > $temp_file
+md5str=`cat $temp_file`
 
 # write to the experiment result
 echo $epochtime $regulartime $md5str >> $output_file
