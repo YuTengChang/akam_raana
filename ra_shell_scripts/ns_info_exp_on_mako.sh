ce805c09075e1bf6f8a7384405b4fe8be48b604d
yutengchang <yutengchang@gmail.com>
2016-03-04 08:54:59 -0500

    for experiments on mako updates remove md5sum

--- ra_shell_scripts/ns_info_exp_on_mako.sh
+++ ra_shell_scripts/ns_info_exp_on_mako.sh
@@ -7,7 +7,7 @@ temp_file="/home/ychang/tmp/ns_exp.txt"
 output_file="/home/ychang/tmp/ns_exp_all.ext"
 
 # get the checksum of the particular mako result
-/home/ychang/bin/mako --csv -s "select sum(demand) from ns_info where COUNTRY='JP' " | tail -1 > $temp_file
+/home/ychang/bin/mako --csv -s "select ip, demand, pingpoint, country, city, asnum, rank() over (partition by country order by demand desc) rankings from ns_info where country='JP' and demand<0.3 and rownum<=1 " | tail -1 > $temp_file
 md5str=`cat $temp_file`
 
 # write to the experiment result
